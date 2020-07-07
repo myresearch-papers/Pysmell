1e71d6222c8d517b099d40846f63e64e55e95a5f
João Correia <jlmc@ic.ufal.br>
2020-07-07 11:53:49 -0300

    Removes files unnecessary for our purposes and leaves the code ready for use.

--- pysmell/detection/example repository/the rest to check/random.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-reader = csv.reader(open("LongParameterList.csv"))
-writer = csv.writer(file('myLongParameterList.csv','wb'))
-
-for line in reader:
-	if reader.line_num%31==0:
-		writer.writerow(line[:-1])
